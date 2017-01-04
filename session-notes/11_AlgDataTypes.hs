955349e9763ce03685672490bd0302ed57843b61
Matthias Heinzel <mheinzel@posteo.de>
2017-01-04 18:25:42 +0100

    Add notes for 11 algebraic datatypes

---
+++ session-notes/11_AlgDataTypes.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- data Maybe a = Nothing | Just { value :: a }
+
+data Present = Pres
+    { presName :: String
+    , presPrice :: Double
+    } deriving Show
+
+data Android = Per Person | Rob Robot
+
+data Person = Person { personName :: String }
+    deriving Show
+
+data Robot = Robot  { robotID :: Int }
+    deriving Show
+
+identification :: Android -> String
+identification (Per p) = personName p
+identification (Rob r) = show $ robotID r
+
+-- these are just aliases, can be used interchangably
+type Address = String
+type Priority = Bool
+
+-- this one is a distinct type
+newtype Name = Name String deriving (Show, Eq)
+-- since Name is a distinct type, it does not reuse String's typeclass instances
+-- (e.g. not not have Ord etc., even though String has).
+-- But we can as usual derive instances and even selectively reuse instances
+-- of the wrapped type using the language extension GeneralizedNewtypeDeriving.
+
+-- You can give any String as Address, any Bool as Priority.
+-- They are only an alias and improve readability, but do not prevent mistakes.
+-- Name, on the other side, cannot be a String!
+f :: Name -> Name -> Address -> Int -> Priority -> String
+f (Name n1) (Name n2) a x b = if b then n1 else n2
+
+
+
+-- ALGEBRAIC?
+
+-- no possible values (identity for sum type), is not inhabited
+data Empty
+
+-- 1 possible value (identity for product type)
+data Something = Something
+
+-- 3 possible values
+data CarManufacturer = Audi | BMW | Mercedes
+
+-- 3 * (1 + 1) possible values
+data Prod = Prod CarManufacturer Bool
+
+-- Prod can be transformed using the distributive law:
+-- 3 * (1 + 1) = 3 + 3 possible values
+-- Prod' it is not identical, but isomophic to Prod.
+data Prod' = ProdTrue CarManufacturer
+           | ProdFalse CarManufacturer
+
+-- 3 * 2 * 1 + 1 = 7 possible values
+data Mixed = Car CarManufacturer Bool Something
+           | NoCar
+
+-- 2 ^ 3 possible values, function acts as exponential
+data CarCheck = Check (CarManufacturer -> Bool)
+
+-- can be transformed as well:
+-- 2 ^ 3 = 2 * 2 * 2
+data CarCheck' = Check' Bool Bool Bool
+-- think of it as a boolean result for each of the 3 possible CarManufacturers
+
